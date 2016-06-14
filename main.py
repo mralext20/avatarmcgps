9b43e36c91c076c4f7e6bb915ca0f5dffa0585c7
Alexander Terry <mralext20@users.noreply.github.com>
2016-06-14 11:47:52 -0800

    added truples.
    you can now input your X, Z coords in a single line.

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import math
-currentX = eval(input("Please enter your current, or starting X coordinate: "))
-currentZ = eval(input("Please enter your current, or starting Z coordinate: "))
+current = eval(input("Please enter your current location, in X, Z format: "))
 
 #above gets current location, bellow finds out where you are trying to get to
 
-targetX = eval(input("Please enter your target, or final X coordinate: "))
-targetZ = eval(input("Please enter your target, or final Z coordinate: "))
+target = eval(input("Please enter your target, in X, Z format: "))
 
 #your bending type, used for finding if /capital is closer.
 
@@ -14,24 +12,19 @@ while (True):
 	bending = bending.upper()
 
 	if bending == "FIRE":
-		capitalX = 3950
-		capitalZ = 9760
+		capital = (3950, 9760)
 		break;
 	elif bending == "AIR":
-		capitalX = 10820
-		capitalZ = 15010
+		capital = (10820, 15010)
 		break;
 	elif bending == "WATER":
-		capitalX = 13300
-		capitalZ = 2030
+		capital = (13300, 2030)
 		break;
 	elif bending == "EARTH":
-		capitalX = 19780
-		capitalZ = 5730
+		capital = (19780, 5730)
 		break;
 	elif bending == "NONE":
-		capitalX = 0
-		capitalZ = 0
+		capital = 0
 		break;
 	else:
 		print("\nYou didn't give a valid input. \n")
@@ -39,10 +32,10 @@ while (True):
 
 #well, now for the hardest part. the distance formula.
 #current position to target.
-targetXDistance = (targetX - currentX)
-targetZDistance = (targetZ - currentZ)
-capitalXDistance = (targetX - capitalX)
-capitalZDistance = (targetZ - capitalZ)
+targetXDistance = (target[0] - current[0])
+targetZDistance = (target[1] - current[1])
+capitalXDistance = (target[0] - capital[0])
+capitalZDistance = (target[1] - capital[1])
 
 targetDistance = math.sqrt(math.pow(targetXDistance,2)+math.pow(targetZDistance,2))
 targetDistance = math.floor(targetDistance)
@@ -50,7 +43,7 @@ print ("Distance from starting point to the target: " + repr(targetDistance))
 
 
 #capital to target. now we check if capitalX is 0, and do not execute this portion.
-if capitalX != 0:
+if capital != 0:
     capitalDistance = math.sqrt(math.pow(capitalXDistance,2)+math.pow(capitalZDistance,2))
     capitalDistance = math.floor(capitalDistance)
     print ("Distance from your capital to the target: " + repr(capitalDistance))
